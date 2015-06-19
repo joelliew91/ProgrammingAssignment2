a79103d6356e337030af9a4f288871a0ef4b0ab8
joelliew91 <joelliew91@gmail.com>
2015-06-19 13:05:33 +0800

    final

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,45 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatirx requires an input matrix to initialise
+## getInv/setInv gets/sets the inverse of the current matrix
+## get/set gets or sets the current matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    
+    i <- NULL
+    
+    setInv<-function(solved) i<<-solved
+    
+    getInv<-function() i
+    
+    set<-function(y){
+        x<<-y
+        i<<-NULL
+    }
+    
+    get<-function() x
+    
+    list(set=set,get=get,getInv=getInv,setInv=setInv)
 
 }
 
 
 ## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix, if the 
+## inverse of the object has yet to be computed
 
 cacheSolve <- function(x, ...) {
+    inv<-x$getInv()
+
+    if(is.null(inv)){
+          temp<-x$get()
+          inv<-solve(temp)
+          x$setInv(inv)
+          x$getInv()
+    }
+    else
+          x$getInv()
+
         ## Return a matrix that is the inverse of 'x'
 }
